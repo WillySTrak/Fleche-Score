0e24d7bde2946c9d6c331424ac45f20bcd097b86
WillySTrak <xazer-zoniak@hotmail.fr>
2026-07-19 17:32:11 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'flech-score-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({type: 'window'}).then((clients) => {
+      if(clients.length > 0) return clients[0].focus();
+      return self.clients.openWindow('./index.html');
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
